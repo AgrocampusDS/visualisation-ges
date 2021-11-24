650583394e90474356c433eea31cd98a54bd08ec
Gaelle <hamelin.jeanpaul@orange.fr>
2021-11-24 21:24:15 +0100

    ajout de 2 graphes : classement par ordre décroissant des pays les plus émetteurs de CO2 (t/hab) en 2016
    évolution depuis 1970 des émissions de CO2 (t/ha) selon pays

---
+++ brouillon.R
@@ -0,0 +1,25 @@
+# Classement des pays les plus émetteurs de CO2 (t/hab) par ordre décroissant en 2016
+
+CO2tot_2016<-temporel %>% filter(Annee=="2016")
+
+graphgen<-CO2tot_2016 %>% 
+  ggplot(aes(fct_rev(fct_reorder(Country.Name,
+                                 TOTAL)),TOTAL))+
+  geom_bar(stat = 'identity',color="pink")+ 
+  labs(x='country',hjust=0.9)+
+  theme(axis.text.x = element_text(face="bold", color="#993333", size=9, angle=90))+
+  labs(y='CO2 (t/hab)',hjust=0.9)
+labs(title='Classement des pays les plus émetteurs de CO2 par ordre décroissant en 2016')
+graphgen
+
+
+# Evolution des émissions de CO2 (t/hab) selon le pays depuis 1970
+temporel %>% filter(Country.Name==c("United States","France","Russia","China",
+                                    "Germany","Korea","Saudi Arabia")) %>% 
+  ggplot()+
+  aes(x=Annee,y=TOTAL,color=Country.Name)+
+  geom_line(size=1)+
+  scale_x_continuous()+
+  ylim(0,31)+
+  ggtitle('Évolution  des émissions de CO2 par pays') +  
+  xlab("année") + ylab("émissions de CO2 t/ hab") 
