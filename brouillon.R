6f14b00ad0b4b770bda2386cf3d62dffb439b0c2
Florence <flo.ghest@gmail.com>
2021-11-25 09:23:33 +0100

    graph 1 , ébauche 1

--- brouillon.R
+++ brouillon.R
@@ -3,6 +3,25 @@ library(ggplot2)
 library(dplyr)
 
 temporel<-read.csv("temporel.csv",header=T,sep=";")
+GES<-read.csv("GES1.csv", header = T, sep=";")
+
+# graphique 1 : proportions mondiales d'émmissions de CO2 par pays 
+
+GES %>% 
+  mutate(tot = CO2_2016+CH4_2016) %>% 
+  mutate(prop = round(((tot/sum(tot)) * 100),1)) %>%
+  arrange(desc(prop)) %>%
+  slice(1:10) %>%
+  ggplot() +
+  aes(x=reorder(Country,(-prop)), y=prop) +
+  geom_bar(stat="identity", fill ="steelblue") +
+  ggtitle("10 pays les plus émetteurs de gazs à effets de serre") +
+  xlab("") +
+  ylab("Part d'émmission de gazs à effets de serre (%)") +
+  geom_text(aes(label=prop), vjust=1.6, color="white", size=3.5)+
+  theme_minimal() 
+  
+
 
 # Classement des pays les plus émetteurs de CO2 (t/hab) par ordre décroissant en 2016
 
